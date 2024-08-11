9feebf7bed722fa6f5146ea8a48026c0316e64b1
Toletum <toletum@gmail.com>
2024-08-11 19:52:26 +0200

    haproxy

--- haproxy/haproxy.sh
+++ haproxy/haproxy.sh
@@ -4,4 +4,4 @@ docker run -d --name haproxy --network host \
 -p 27017:27017 -p 27018:27018 -p 27019:27019 \
 -v ./haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
 haproxy:alpine \
-haproxy -f /usr/local/etc/haproxy/haproxy.cfgy
+haproxy -f /usr/local/etc/haproxy/haproxy.cfg
